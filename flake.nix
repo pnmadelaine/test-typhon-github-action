665d99df3bab8fdeb3bdcd9700f83082000d914a
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-17 12:38:27 +0200

    test

--- flake.nix
+++ flake.nix
@@ -10,21 +10,30 @@
     {
       typhonJobs.${system}.hello = pkgs.writeTextDir "hello.txt" "Hello world!";
       typhonProject = typhon.lib.builders.mkProject {
-        actions.end = typhon.lib.compose.match [
-          {
-            jobset = "main";
-            inherit system;
-            job = "hello";
-            action = typhon.lib.github.mkPushResult {
-              owner = "pnmadelaine";
-              repo = "test-typhon-github-action";
-              branch = "hello";
-            };
+        actions.end = typhon.lib.builders.mkActionScript (
+          { pkgs, system }: {
+            path = [ pkgs.jq ];
+            script = ''
+              stdin=$(cat)
+              echo "$stdin" | >&2 jq 'keys[]'
+            '';
           }
-          {
-            action = typhon.lib.builders.mkDummyAction { output = "null"; };
-          }
-        ];
+        );
+        #actions.end = typhon.lib.compose.match [
+        #  {
+        #    jobset = "main";
+        #    inherit system;
+        #    job = "hello";
+        #    action = typhon.lib.github.mkPushResult {
+        #      owner = "pnmadelaine";
+        #      repo = "test-typhon-github-action";
+        #      branch = "hello";
+        #    };
+        #  }
+        #  {
+        #    action = typhon.lib.builders.mkDummyAction { output = "null"; };
+        #  }
+        #];
       };
     };
 }
